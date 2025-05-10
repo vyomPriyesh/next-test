eef8bd32304ec423143bdd1a5d37fe8dc58917f7
vyomPriyesh <vyompriyesh@gmail.com>
2025-05-10 15:10:20 +0530

    add

--- src/app/ctg/[id]/page.tsx
+++ src/app/ctg/[id]/page.tsx
@@ -8,7 +8,7 @@ async function getMetaFromServer() {
   return {
     title: 'Category Title',
     description: 'Description for this category',
-    image: 'https://example.com/images/category.jpg',
+    image: 'https://img.youtube.com/vi/sgfe95B8V6w/hqdefault.jpg',
   };
 }
 
