fc898eff5052b7cf753f44f34628db0e46bcda23
Priyesh Bhanderi <priyesh.bhanderi@genixbit.com>
2025-05-10 21:31:16 +0530

    Add

--- src/app/ctg/[slug]/page.tsx
+++ src/app/ctg/[slug]/page.tsx
@@ -1,22 +1,23 @@
 import Ctg from '@/components/Ctg';
 import { getMetaFromServer } from '@/lib/getMeta';
 import { Metadata } from 'next';
+import { type PageProps } from 'next/app';
 
 // Type for dynamic route parameters
 interface Params {
   slug: string;
 }
 
-// Generate metadata
-export async function generateMetadata({
-  params,
-}: {
+interface Props extends PageProps {
   params: Params;
-}): Promise<Metadata> {
+}
+
+// Generate metadata
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
   return getMetaFromServer(params.slug);
 }
 
 // Page component
-export default function Page({ params }: { params: Params }) {
+export default function Page({ params }: Props) {
   return <Ctg id={params.slug} />;
 }
