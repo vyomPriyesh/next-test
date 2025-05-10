799bc650f6678e9fdb920016aa9ae4d501327338
Priyesh Bhanderi <priyesh.bhanderi@genixbit.com>
2025-05-10 20:55:51 +0530

    Add

--- src/app/ctg/[slug]/page.tsx
+++ src/app/ctg/[slug]/page.tsx
@@ -2,14 +2,25 @@ import { getMetaFromServer } from '@/lib/getMeta';
 import Ctg from '@/components/Ctg';
 import { Metadata } from 'next';
 
-export async function generateMetadata(
-  { params }: { params: { slug: string } }
-): Promise<Metadata> {
+// Define the expected params type
+interface PageParams {
+  slug: string;
+}
+
+// Generate metadata
+export async function generateMetadata({
+  params,
+}: {
+  params: PageParams;
+}): Promise<Metadata> {
   return await getMetaFromServer(params.slug);
 }
 
-export default function Page(
-  { params }: { params: { slug: string } }
-) {
+// Page component
+export default function Page({
+  params,
+}: {
+  params: PageParams;
+}) {
   return <Ctg id={params.slug} />;
 }
