bfcb15f84c5a787fe55432a3319b57bad4aa2037
Priyesh Bhanderi <priyesh.bhanderi@genixbit.com>
2025-05-11 18:28:53 +0530

    Add

--- src/app/ctg/[id]/page.tsx
+++ src/app/ctg/[id]/page.tsx
@@ -21,22 +21,22 @@ export async function generateMetadata({
   }
 }
 
-export default async function CtgPage({
-  params,
-}: {
-  params: { id: string }
-}) {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}news_details/1/${params.id}`, {
-    cache: 'no-store',
-  })
+// export default async function CtgPage({
+//   params,
+// }: {
+//   params: { id: string }
+// }) {
+//   const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}news_details/1/${params.id}`, {
+//     cache: 'no-store',
+//   })
 
-  const json = await res.json()
-  const news = json.data
+//   const json = await res.json()
+//   const news = json.data
 
-  return (
-    <div>
-      <h1>{news.title}</h1>
-      <p>{news.content}</p>
-    </div>
-  )
-}
+//   return (
+//     <div>
+//       <h1>{news.title}</h1>
+//       <p>{news.content}</p>
+//     </div>
+//   )
+// }
