d1dfa1062dce97129ce76aee5515be36e2c4afc2
Priyesh Bhanderi <priyesh.bhanderi@genixbit.com>
2025-05-11 18:23:06 +0530

    Add

--- src/app/ctg/[id]/page.tsx
+++ src/app/ctg/[id]/page.tsx
@@ -1,35 +1,42 @@
+// app/ctg/[id]/page.tsx
+
 import { Metadata } from 'next'
 
-export async function generateMetadata(
-  { params }: { params: { id: string } }
-): Promise<Metadata> {
+// `generateMetadata` will automatically infer the `params` type correctly
+export async function generateMetadata({
+  params,
+}: {
+  params: { id: string }
+}): Promise<Metadata> {
   const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}news_details/1/${params.id}`, {
     cache: 'no-store',
-  });
+  })
 
-  const json = await res.json();
-  const title = json?.data?.title || 'Default Title';
+  const json = await res.json()
+  const title = json?.data?.title || 'Default Title'
 
   return {
     title,
     description: `Read more about: ${title}`,
-  };
+  }
 }
 
-export default async function CtgPage(
-  { params }: { params: { id: string } }
-) {
+export default async function CtgPage({
+  params,
+}: {
+  params: { id: string }
+}) {
   const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}news_details/1/${params.id}`, {
     cache: 'no-store',
-  });
+  })
 
-  const json = await res.json();
-  const news = json.data;
+  const json = await res.json()
+  const news = json.data
 
   return (
     <div>
-      <h1>{news.title}a</h1>
+      <h1>{news.title}</h1>
       <p>{news.content}</p>
     </div>
-  );
+  )
 }
