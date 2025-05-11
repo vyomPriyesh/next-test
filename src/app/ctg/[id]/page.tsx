e2be9fe5adf7d8eca0d33d13d908c9fe48d7d8e6
Priyesh Bhanderi <priyesh.bhanderi@genixbit.com>
2025-05-11 17:01:48 +0530

    Add

--- src/app/ctg/[id]/page.tsx
+++ src/app/ctg/[id]/page.tsx
@@ -1,35 +1,39 @@
 'use client'
 
+import { setError, setLoading, setNewsData } from '@/lib/slices/newsSlice';
 import axios from 'axios';
-import { useParams } from 'next/navigation'
+import { useParams } from 'next/navigation';
 import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 
 export default function CtgPage() {
   const { id } = useParams();
-
-  if (!id) {
-    return <p>Loading…</p>
-  }
-
+  const dispatch = useDispatch();
   const apiUrl = process.env.NEXT_PUBLIC_API_URL;
 
-
   useEffect(() => {
+    if (!id) return;
 
     const getData = async () => {
+      dispatch(setLoading(true));
       try {
         const response = await axios.get(`${apiUrl}news_details/1/${id}`);
+        dispatch(setNewsData(response.data));
       } catch (err) {
-        console.log(err)
+        dispatch(setError(err.message));
+      } finally {
+        dispatch(setLoading(false));
       }
-    }
-    getData()
-  }, [id])
+    };
 
+    getData();
+  }, [id]);
+
+  if (!id) return <p>Loading…</p>;
 
   return (
     <div>
       <h1>Category ID: {id}</h1>
     </div>
-  )
+  );
 }
