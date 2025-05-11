fb61f7421f580d7ae16234c17935b85655e475c4
Priyesh Bhanderi <priyesh.bhanderi@genixbit.com>
2025-05-11 18:45:18 +0530

    Add

--- src/app/ctg/[id]/page.tsx
+++ src/app/ctg/[id]/page.tsx
@@ -6,34 +6,39 @@ import { Metadata } from 'next'
 type Params = Promise<{ id: string }> // `id` is a single string, not an array
 
 export async function generateMetadata({ params }: { params: Params }): Promise<Metadata> {
-   const { id } = await params // Directly access `id`
+  const { id } = await params
   const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}news_details/1/${id}`, {
-    cache: 'no-store', // Ensure no caching during build
+    cache: 'no-store',
   });
 
   const json = await res.json();
   const title = json?.data?.title || 'Default Title';
+  const image = `https://img.youtube.com/vi/${json?.data?.blog_image[0]?.details}/hqdefault.jpg`
 
   return {
     title,
     description: `Read more about: ${title}`,
+    image: image
   };
 }
 
 export default async function CtgPage({ params }: { params: Params }) {
-  const { id } = await params;  // Directly access `id`
+  const { id } = await params;
 
   const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}news_details/1/${id}`, {
-    cache: 'no-store', // Ensure no caching during build
+    cache: 'no-store',
   });
 
   const json = await res.json();
   const news = json.data;
+  const image = `https://img.youtube.com/vi/${json?.data?.blog_image[0]?.details}/hqdefault.jpg`
+
 
   return (
     <div>
       <h1>{news.title}</h1>
       <p>{news.content}</p>
+      <img src={image} alt="" />
     </div>
   );
 }
