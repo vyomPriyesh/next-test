34f3df050e42db20945c8b2c7a929522620a6dc5
Priyesh Bhanderi <priyesh.bhanderi@genixbit.com>
2025-05-21 21:12:40 +0530

    Add

--- src/app/our-board/[id]/page.tsx
+++ src/app/our-board/[id]/page.tsx
@@ -2,13 +2,13 @@
 import { useParams } from 'next/navigation'
 import React from 'react'
 
-const page = () => {
+const Page = () => {
 
     const { id } = useParams();
 
     return (
-        <div>page our {id}</div>
+        <div>Page our {id}</div>
     )
 }
 
-export default page
+export default Page
